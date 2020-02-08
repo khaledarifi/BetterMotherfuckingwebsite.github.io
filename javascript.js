5a2630e1cee322582ec6351356e2e0fe3f54861e
khaledarifi <60709157+khaledarifi@users.noreply.github.com>
2020-02-08 17:50:01 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+
+
+
+setTimeout(function() {
+	if(confirm('Hey le site xxxvidsxx est trop bien Viens dessus stp please. Voulez-vous aller sur xxxvidsxx'))
+{window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+	
+},10000);
+		
+	function modePsychedelique() {
+		
+	document.body.style.backgroundColor="magenta"
+	var tab=document.getElementsByTagName("p")
+	for(var i=0; i<tab.length ; i++)
+	{
+		tab[i].style.color="blue"
+		tab[i].style.fontFamily="courier"
+	}		
+	
+	var all_headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6")
+	for(var i=0; i<all_headings.length ; i++)
+		all_headings[i].style.color="green"
+	
+	var all_headers=document.getElementsByTagName("header")
+	for(var i=0; i<all_headers.length ; i++)
+		all_headers[i].style.fontFamily="comic sans ms"
+	}
+	
